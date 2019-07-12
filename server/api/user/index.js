7f8a302db84ca3a34f3f0848068844beca6f525b
Nishant Tanwar <nishantt12@gmail.com>
2019-07-12 15:23:26 +0530

    updated api paths

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -9,17 +9,17 @@ router.get('/', controller.index);
 router.get('/:id', controller.show);
 
 router.delete('/:id', controller.destroy);
-router.delete('/delete/deleteAll', controller.deleteAll);
+router.delete('/', controller.deleteAll);
 
 
 router.get('/me/:id', auth.isAuthenticated(), controller.getMe);
-router.post('/create', controller.create);
-router.post('/userExists', controller.userExists);
-router.post('/userValidation', controller.userValidation);
+router.post('/', controller.create);
+router.post('/exists', controller.userExists);
+router.post('/valid', controller.userValidation);
 
-router.post('/verifyOtp', controller.verifyOtp);
-router.post('/resendOtp', controller.resendOtp);
-router.post('/createOtp', controller.createPhoneOtp);
+router.post('/otp/verify', controller.verifyOtp);
+router.post('/otp/resend', controller.resendOtp);
+router.post('/otp', controller.createPhoneOtp);
 router.post('/login', controller.login);
 
 module.exports = router;
